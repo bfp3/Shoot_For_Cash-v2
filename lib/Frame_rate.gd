01ad4019548eddf0b7bf45062af22283293a13ab
bfp3 <92760649+bfp3@users.noreply.github.com>
2026-06-23 16:52:38 +1200

    push before deleting untiitled

--- lib/Frame_rate.gd
+++ lib/Frame_rate.gd
@@ -6,3 +6,7 @@ func _on_timer_timeout() -> void:
 	var draw_calls = Performance.get_monitor(Performance.RENDER_TOTAL_DRAW_CALLS_IN_FRAME)
 
 	$Label.text = str(fps) + "\nDRAW CALLS: " + str(draw_calls)
+
+func _input(event: InputEvent) -> void:
+	if Input.is_key_label_pressed(KEY_0):
+		visible = !visible
