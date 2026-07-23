bec0698254ba8eeb1c7275c7f7d961b20c5e9e1f
bfp3 <92760649+bfp3@users.noreply.github.com>
2026-07-23 15:55:05 +1200

    push

---
+++ ch/HUD_components/strike_indicator.gd
@@ -0,0 +1,7 @@
+extends Control
+
+@onready var size_control: Control = $Control
+
+@onready var circle: TextureRect = $Control/Circle
+@onready var cross: TextureRect = $Control/Cross
+@onready var cross_front: TextureRect = $Control/CrossFront
