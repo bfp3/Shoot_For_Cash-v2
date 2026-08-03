0bd98cbac10bec88cd52af1cd31d443cce7e9512
bfp3 <92760649+bfp3@users.noreply.github.com>
2026-08-04 08:54:33 +1200

    push before cursor

--- lib/HUD_Feedback_Corner_Folder/Scripts/game_won_special_yellow.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func display_tally() -> void:
-	var tween = create_tween().set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(self, "modulate", Color('FFFFFF79'), 0.5)
-	tween.parallel().tween_property($Scale_control/Light, "modulate", Color.GOLD, 0.5)
-	await tween.finished
